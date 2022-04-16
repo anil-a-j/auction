0eb30121f328550bb6733d74fb42eae29b30f2a6
unknown <anilaloorjoseph@gmail.com>
2022-04-16 11:02:38 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -48,4 +48,7 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT);
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`)
+);
